36ec4d3fcb6c62a9c3ff09b353d81a7f09bf70e8
Adam Good <dev.adamgood@gmail.com>
2021-12-24 10:35:19 -0700

    Added window sum functionality

--- problem_1/sonar.py
+++ problem_1/sonar.py
@@ -19,10 +19,16 @@ def read_measurments(filename: str) -> list[int]:
         measurementes = [int(line) for line in input_file]
     return measurementes
 
-def count_increases(measurements: list[int]) -> int:
-    return sum([1 for a,b in zip(measurements[:-1], measurements[1:]) if (b-a) > 0])
+def get_windows(data: list[int], window_size: int) -> list[list[int]]:
+    return [data[i:i+window_size] for i in range(len(data)-window_size+1)]
+
+def sum_windows(windows: list[list[int]]) -> list[int]:
+    return [sum(win) for win in windows]
+
+def count_increases(data: list[int]) -> int:
+    return sum([1 for a,b in zip(data[:-1], data[1:]) if (b-a) > 0])
 
 if __name__ == "__main__":
     filename = get_input_filename()
     measurements = read_measurments(filename)
-    print(count_increases(measurements))
+    print(count_increases(sum_windows(get_windows(measurements, 3))))
