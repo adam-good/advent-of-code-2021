0f9bfb33d29256424155911f6fbfa0c7a3a81d15
Adam Good <dev.adamgood@gmail.com>
2021-12-23 19:07:25 -0700

    Just some refactoring

--- problem_1/sonar.py
+++ problem_1/sonar.py
@@ -5,19 +5,28 @@ import time
 import multiprocess
 import math
 
+def get_input_filename() -> str:
+    # NOTE: Argparse is probably overkill for one positional argument but oh well
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_file', nargs='?', default='./input.txt')
+
+    args = parser.parse_args()
+    filename = args.input_file
+    return filename
+
 def read_measurments(filename: str) -> list[int]:
     with open(filename) as input_file:
         measurementes = [int(line) for line in input_file]
     return measurementes
 
-def count_increases_imparative(measurements: list[int]) -> int:
+def _count_increases_imparative(measurements: list[int]) -> int:
     num_increases = 0
     for i in range(1,len(measurements)):
         if measurements[i] > measurements[i-1]:
             num_increases += 1
     return num_increases
 
-def count_increases_functional(measurements: list[int]) -> int:
+def _count_increases_functional(measurements: list[int]) -> int:
     return sum([1 for a,b in zip(measurements[:-1], measurements[1:]) if (b-a) > 0])
 
 def _time_function(fn, verbose=False) -> float:
@@ -29,7 +38,7 @@ def _time_function(fn, verbose=False) -> float:
 
     return end_time 
 
-def _compare_functions():
+def _plot_compare_functions():
     points_imp = []
     points_fnc = []
     scales = [2**a for a in range(26)]
@@ -50,8 +59,8 @@ def _compare_functions():
     for s in scales:
         print(f"{math.log(s,2)}", end='\r')
         t1 = time.time()
-        sample_imp = sample_fn(count_increases_imparative, s, 16)
-        sample_fnc = sample_fn(count_increases_functional, s, 16)
+        sample_imp = sample_fn(_count_increases_imparative, s, 16)
+        sample_fnc = sample_fn(_count_increases_functional, s, 16)
         t_diff = time.time() - t1
         points_imp.append(sample_imp)
         points_fnc.append(sample_fnc)
@@ -65,17 +74,8 @@ def _compare_functions():
     plt.savefig('./output/scaling_study.png')
     plt.show()
 
-def get_input_filename() -> str:
-    # NOTE: Argparse is probably overkill for one positional argument but oh well
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input_file', nargs='?', default='./input.txt')
-
-    args = parser.parse_args()
-    filename = args.input_file
-    return filename
-
 if __name__ == "__main__":
     filename = get_input_filename()
     measurements = read_measurments(filename)
-    print(count_increases_imparative(measurements))
-    print(count_increases_functional(measurements))
+    print(_count_increases_imparative(measurements))
+    print(_count_increases_functional(measurements))
